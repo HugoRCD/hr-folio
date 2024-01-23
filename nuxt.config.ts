d35cce9df15e51f805090d8a5161c300d68fa58a
HugoRCD <hugo.richard@epitech.eu>
2024-01-23 17:08:32 +0100

    Add Nuxt content and robots.txt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['blanked'],
+  modules: ['blanked', '@nuxt/content'],
+
+  colorMode: {
+    classSuffix: '',
+    preference: 'light',
+  },
+
   css: ['~/main.css'],
+
   devtools: { enabled: true }
 })
