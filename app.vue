36dd4d63777c03faab603a2370324cfa31edcde3
HugoRCD <hugo.richard@epitech.eu>
2024-01-23 16:10:25 +0100

    Better layout

--- app.vue
+++ app.vue
@@ -1,11 +1,17 @@
 <template>
-  <Html>
-    <Body class="relative dark:overflow-hidden h-screen transition-colors duration-300 ease-in-out bg-light dark:bg-dark">
+  <Html class="overflow-hidden">
+    <Body class="relative overflow-hidden h-screen transition-colors duration-300 ease-in-out bg-light dark:bg-dark">
+      <div class="absolute size-72 bg-dark dark:bg-light blur-[200px] dark:blur-[200px] rounded-full z-40 -top-56" />
       <div
-        class="noise z-50 pointer-events-none bg-left-top absolute inset-[-200%] h-[400%] w-[400%] bg-[url('/noise.png')] opacity-[10%]"
+        class="noise z-50 pointer-events-none absolute inset-[-200%] h-[400%] w-[400%] bg-[url('/noise.png')] opacity-[10%]"
       />
-      <ThemeSelector class="absolute top-2 right-2 z-50" />
-      <NuxtPage />
+      <div class="flex flex-col justify-center items-center gap-3 p-6 sm:p-12 h-full">
+        <div class="relative h-full p-8 border-2 border-dark/20 dark:border-light/20 flex flex-col justify-center gap-3 w-full">
+          <ThemeSelector class="absolute top-4 right-4 z-50" />
+          <NuxtPage />
+          <Signature class="absolute bottom-4 right-4 z-40 h-20 fill-black dark:fill-white" />
+        </div>
+      </div>
     </Body>
   </Html>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,32 +3,31 @@ const { socials } = useAppConfig();
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center gap-3 p-12 h-full">
-    <div class="relative h-full p-8 border-2 border-dark/20 dark:border-light/20 flex flex-col justify-center gap-3 w-full">
-      <div class="size-52 bg-white blur-[200px] rounded-full z-40 absolute -top-56" />
-      <h1 class="text-4xl font-newsreader italic">
-        Hi i'm Hugo
-      </h1>
-      <p class="text-balance max-w-md">
-        Developer and <span class="underline font-bold text-accent">designer</span>, i like to make things, lots of
-        things and i'm pretty good at it
-      </p>
-      <p class="blurred">
-        But also a fucking genius
-      </p>
-      <div class="mt-4 flex flex-col gap-2 w-fit">
-        <NuxtLink
-          v-for="social in socials"
-          :key="social.name"
-          class="link group cursor-pointer font-newsreader flex items-center gap-1 text-xl"
-          :to="social.link"
-          target="_blank"
-        >
-          {{ social.name }}<span class="i-lucide-arrow-up-right font-light hidden group-hover:block" />
-        </NuxtLink>
-      </div>
-      <Signature class="absolute bottom-4 right-4 z-40 h-20 fill-black dark:fill-white" />
-    </div>
+  <div class="absolute size-52 bg-white blur-[200px] rounded-full z-40 -top-56" />
+  <div class="flex flex-col">
+    <h2 class="text-lg font-newsreader italic">
+      Hugo Richard
+    </h2>
+    <h1 class="text-3xl font-newsreader italic text-accent">
+      Developer and Designer
+    </h1>
+  </div>
+  <p class="text-balance font-light max-w-lg">
+    I like to make things, lots of things and i'm pretty good at it.
+  </p>
+  <p class="blurred w-fit">
+    But also a fucking genius
+  </p>
+  <div class="mt-4 flex flex-col gap-2 w-fit">
+    <NuxtLink
+      v-for="social in socials"
+      :key="social.name"
+      class="link group cursor-pointer font-newsreader flex items-center gap-1 text-xl"
+      :to="social.link"
+      target="_blank"
+    >
+      {{ social.name }}<span class="i-lucide-arrow-up-right font-light hidden group-hover:block" />
+    </NuxtLink>
   </div>
 </template>
 
