2022104da0a1a9361fa59f9bae9dea498ed0e4e3
HugoRCD <hugo.richard@epitech.eu>
2024-01-23 20:47:43 +0100

    Fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   root: true,
   extends: ["@nuxt/eslint-config"],
 };
--- pages/writing/[slug].vue
+++ pages/writing/[slug].vue
@@ -3,13 +3,16 @@
 </script>
 
 <template>
-  <div class="writing">
-    <NuxtLink to="/" class="cursor-pointer group absolute top-4 left-8 z-50 no-underline">
+  <div>
+    <NuxtLink
+      to="/"
+      class="cursor-pointer group absolute top-4 left-8 z-50 no-underline"
+    >
       <span class="font-newsreader italic hover:text-accent">
         go back
       </span>
     </NuxtLink>
-    <ContentDoc />
+    <ContentDoc class="writing" />
   </div>
 </template>
 
