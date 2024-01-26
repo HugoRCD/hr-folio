5dbadae8cdc0cfe9f99aaa2d728832a14707470b
HugoRCD <hugo.richard@epitech.eu>
2024-01-26 18:52:52 +0100

    Ad vercel analytics

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
+
 <template>
   <Html lang="en">
     <Body class="relative">
+      <SpeedInsights />
       <div class="absolute size-44 sm:size-72 opacity-50 dark:bg-light blur-[200px] dark:blur-[200px] rounded-full z-40 -top-56" />
       <div class="pointer-events-none fixed inset-0 z-40 h-full w-full overflow-hidden">
         <div class="absolute noise z-50 pointer-events-none inset-[-200%] size-[400%] bg-[url('/noise.png')] opacity-[7%]" />
