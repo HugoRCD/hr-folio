7c1f5b8dccc416cedeb502129d4144572ab10f60
HugoRCD <hugo.richard@epitech.eu>
2024-05-05 21:58:11 +0200

    add new layout

---
+++ layouts/note.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/nuxt'
+const { link } = useAppConfig()
+
+useHead({
+  titleTemplate: '',
+  htmlAttrs: {
+    lang: 'en',
+  },
+  link,
+  script: [
+    {
+      src: 'https://plausible.hrcd.fr/js/script.js',
+      defer: true,
+      'data-domain': 'hrcd.fr'
+    },
+  ],
+})
+
+const { lessThan } = useWindowInfos()
+</script>
+
+<template>
+  <div class="relative">
+    <SpeedInsights />
+    <div class="pointer-events-none absolute -top-56 z-40 size-44 rounded-full opacity-50 blur-[200px] dark:bg-white dark:blur-[200px] sm:size-72" />
+    <div class="pointer-events-none fixed inset-0 z-40 size-full overflow-hidden">
+      <div class="noise pointer-events-none absolute inset-[-200%] z-50 size-[400%] bg-[url('/noise.png')] opacity-[4%]" />
+    </div>
+    <main class="bg-light dark:bg-dark flex min-h-screen flex-col items-center justify-center p-4 sm:p-12">
+      <LayoutLetterCard>
+        <LayoutNavbar />
+        <ContentDoc class="writing mb-4 mt-8">
+          <template #not-found>
+            <DocumentDrivenNotFound />
+          </template>
+        </ContentDoc>
+        <LayoutFooter />
+      </LayoutLetterCard>
+    </main>
+    <Toasts
+      :position="lessThan('sm') ? 'bottom-center' : 'top-center'"
+      close-button
+      :toast-options="{
+        style: {
+          backgroundColor: 'var(--primary-color)'
+        }
+      }"
+    />
+  </div>
+</template>
+
