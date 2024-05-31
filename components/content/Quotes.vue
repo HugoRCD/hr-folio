6be1854d81498df52f4b515eac5530e77d5f576e
HugoRCD <hugo.richard@epitech.eu>
2024-05-31 14:01:06 +0200

    Init quotes section

---
+++ components/content/Quotes.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const { data } = await useAsyncData('quotes', () =>
+  queryContent('quotes')
+    .where({ _type: 'json' })
+    .findOne()
+)
+</script>
+
+<template>
+        <div class="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2">
+            <div
+            v-for="(quote, index) in data!.quotes"
+                :key="quote._id"
+            class="mb-4"
+            data-animate
+            :style="{ '--stagger': index }"
+            >
+                <h4 class="font-newsreader text-lg italic ">
+                "{{ quote.text }}"
+                </h4>
+                <p class="text-sm text-gray-500">
+                {{ quote.author }}
+                </p>
+            </div>
+        </div>
+</template>
