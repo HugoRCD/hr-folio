c46f68ed2b145c3e029a7b7947cbb7f16ff5f281
HugoRCD <hugo.richard@epitech.eu>
2024-07-18 09:27:54 +0200

    remove compatibility key

--- .config/nuxt.ts
+++ .config/nuxt.ts
@@ -3,8 +3,6 @@ export default defineNuxtConfig({
     compatibilityVersion: 4
   },
 
-  compatibilityDate: '2024-04-03',
-
   app: {
     head: {
       viewport: 'width=device-width, initial-scale=1',
