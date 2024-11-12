8f56e9f7f835f4cb4635460e79a1916a9aa50152
HugoRCD <hugo.richard@epitech.eu>
2024-11-12 20:04:15 +0100

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -74,7 +74,7 @@ export default defineNuxtConfig({
   },
 
   nitro: {
-    preset: process.env.NITRO_PRESET || 'node-server',
+    preset: process.env.NITRO_PRESET || 'bun',
     prerender: {
       crawlLinks: true,
       routes: ['/', '/works', '/about', '/writing', '/sitemap.xml'],
