4e6bc3e3f873784b53dec864396565df91b2252a
HugoRCD <hugo.richard@epitech.eu>
2024-12-05 14:32:28 +0100

    wip

--- app/components/toc/Links.vue
+++ app/components/toc/Links.vue
@@ -8,6 +8,7 @@ type ContentTocProps = {
   title?: string
   links: TocLink[]
   active?: string
+  isHover: boolean
 }
 
 
@@ -33,18 +34,46 @@ const scrollToHeading = (id: string): void => {
 </script>
 
 <template>
-  <ul v-if="links?.length">
-    <li v-for="link in links" :key="link.text" :class="link.depth === 3 ? 'ml-3' : ''">
-      <a
-        class="block truncate text-xs/6"
-        :class="activeHeadings.includes(link.id) ? 'text-accent' : 'text-tertiary hover:text-gray-700 dark:hover:text-gray-200'"
-        :href="`#${link.id}`"
-        @click.prevent="scrollToHeading(link.id)"
+  <div>
+    <ul v-if="links?.length && isHover" class="space-y-2">
+      <li
+        v-for="link in links"
+        :key="link.text"
+        :class="[
+          link.depth === 3 ? 'ml-4' : ''
+        ]"
       >
-        {{ link.text }}
-      </a>
+        <a
+          class="block truncate text-xs/6"
+          :class="activeHeadings.includes(link.id) ? 'text-accent' : 'text-tertiary hover:text-gray-700 dark:hover:text-gray-200'"
+          :href="`#${link.id}`"
+          @click.prevent="scrollToHeading(link.id)"
+        >
+          {{ link.text }}
+        </a>
 
-      <Links v-if="link.children" :links="link.children" />
-    </li>
-  </ul>
+        <Links v-if="link.children" :links="link.children" :is-hover />
+      </li>
+    </ul>
+    <div v-else-if="links?.length" class="space-y-4">
+      <div
+        v-for="link in links"
+        :key="link.text"
+        class="flex flex-col items-end gap-1"
+      >
+        <div
+          class="h-[4px] rounded-full transition-all duration-200 cursor-pointer"
+          :class="[
+            activeHeadings.includes(link.id)
+              ? 'w-12 bg-accent'
+              : 'w-8 bg-secondary/20',
+            link.depth === 3 ? 'w-6' : ''
+          ]"
+          @click="scrollToHeading(link.id)"
+        />
+
+        <Links v-if="link.children" :links="link.children" :is-hover />
+      </div>
+    </div>
+  </div>
 </template>
