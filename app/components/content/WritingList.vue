f484cff4330762b868d7844da0106ed494e5d23d
HugoRCD <hugo.richard@epitech.eu>
2025-03-03 21:59:15 +0100

    feat: update ui-pro and content

--- app/components/content/WritingList.vue
+++ app/components/content/WritingList.vue
@@ -23,7 +23,7 @@ if (!data.value || !error.value) createError({ statusCode: 404 })
     </NuxtLink>
     <NuxtLink
       to="/writing"
-      class="mt-2 link font-serif text-lg hover:underline"
+      class="mt-2 italic font-serif text-lg hover:underline decoration-accent"
       aria-label="See more, go to all articles, writing, etc ..."
     >
       <span class="sr-only">More writing</span>
