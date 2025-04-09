fec22d8d95f026553fa6557d71325ed55c084f98
HugoRCD <hugo.richard@epitech.eu>
2025-04-09 17:36:30 +0200

    feat: migrate to nuxthub

--- app/components/content/Hero.vue
+++ app/components/content/Hero.vue
@@ -1,24 +1,27 @@
 <script setup lang="ts">
-const runtimeConfig = useRuntimeConfig()
-const { meetingLink } = runtimeConfig.public
-const { profile } = useAppConfig()
+const { profile, global } = useAppConfig()
 </script>
 
 <template>
   <div class="flex flex-col" data-animate style="--stagger: 1">
-    <NuxtImg
-      :src="profile.picture"
-      alt="Hugo Richard"
-      width="80"
-      height="80"
-      sizes="160"
-      class="mb-2 size-20 rounded-full object-cover"
-    />
-    <h3 class="font-serif text-xl italic">
-      <slot name="title" mdc-unwrap="p" />
-    </h3>
-    <div class="*:mb-3 *:text-(--ui-primary)">
-      <slot name="job" mdc-unwrap="p" />
+    <div class="flex items-center gap-2 mb-2">
+      <NuxtImg
+        :src="profile.picture"
+        alt="Hugo Richard"
+        width="80"
+        height="80"
+        sizes="160"
+        format="webp"
+        class="mb-2 size-18 rounded-full object-cover"
+      />
+      <div class="flex flex-col">
+        <h3 class="font-serif text-xl italic">
+          <slot name="title" mdc-unwrap="p" />
+        </h3>
+        <div class="*:mb-0 *:text-(--ui-primary)">
+          <slot name="job" mdc-unwrap="p" />
+        </div>
+      </div>
     </div>
     <p class="max-w-[600px] text-pretty text-(--ui-text-muted) text-sm font-extralight sm:text-base">
       <slot mdc-unwrap="p" />
@@ -27,7 +30,7 @@ const { profile } = useAppConfig()
       <NuxtLink to="/contact" class="italic font-serif text-lg hover:underline decoration-accent">
         Send me a message
       </NuxtLink>
-      <NuxtLink :to="meetingLink" class="italic font-serif text-lg hover:underline decoration-accent cursor-pointer">
+      <NuxtLink :to="global.meetingLink" class="italic font-serif text-lg hover:underline decoration-accent cursor-pointer">
         Make a call
       </NuxtLink>
     </div>
